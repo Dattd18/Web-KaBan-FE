bec40a8598e24562a5eb296552912e50216c75c8
DatTD18 <tranduydat19082003@gmail.com>
2025-10-06 08:56:33 +0700

    update code

--- src/context/authcontext.js
+++ src/context/authcontext.js
@@ -41,6 +41,7 @@ export const AuthProvider = ({ children }) => {
 
     const logout = () => {
         localStorage.removeItem("token");
+        localStorage.removeItem("userId");
         setRole(null);
         setIsAuthenticated(false);
     };
